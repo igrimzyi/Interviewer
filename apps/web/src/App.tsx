fd5e8c003c12d54b5d295ac90ae697ec17cb4305
isaiahtamayo <isaiah.tamayo@springscs.org>
2026-03-04 22:36:22 -0800

    Removed forced redirect

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -4,9 +4,6 @@ import Dashboard from "./pages/Dashboard";
 function App() {
   return (
     <Routes>
-      {/* Redirect root to dashboard */}
-      <Route path="/" element={<Navigate to="/dashboard" replace />} />
-
       {/* Only actual page */}
       <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
