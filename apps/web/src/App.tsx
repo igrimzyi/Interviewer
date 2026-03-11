d50633619299406c0c68321aa6517aa5a973327f
ben482901 <benjaminharrity@gmail.com>
2026-03-10 19:09:02 -0700

    Add login page and login route

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,19 +1,20 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar.tsx'
 import './index.css'
 import LandingPage from './pages/LandingPage.tsx'
 
 import Dashboard from "./pages/Dashboard";
+import LoginPage from "./pages/LoginPage.tsx";
 
 function App() {
   return (
     <div className="min-h-screen w-full">
-    <Routes>
-      <Route path="/" element={<><Navbar/> <LandingPage /></>} />
-      <Route path="/dashboard" element={<Dashboard />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<><Navbar /> <LandingPage /></>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </div>
-    
   );
 }
 
