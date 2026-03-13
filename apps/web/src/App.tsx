cd792750ed1c5f15d09df6fcedbfa204a8433f47
Shaneo <shaneshack10@icloud.com>
2026-03-12 21:32:18 -0700

    feat(register): add /register route

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,7 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Navbar from './components/Navbar.tsx'
 import './index.css'
 import LandingPage from './pages/LandingPage.tsx'
-
+import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
@@ -10,6 +10,7 @@ function App() {
     <div className="min-h-screen w-full">
     <Routes>
       <Route path="/" element={<><Navbar/> <LandingPage /></>} />
+      <Route path="/register" element={<Register />} />
       <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
     </div>
